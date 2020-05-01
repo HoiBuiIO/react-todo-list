16e4890effc7b490b6b8a9abdb3ac8da3ab3098d
hoibuikms <hoibui@kms-technology.com>
2020-05-01 14:37:12 +0700

    fix edit and delete mode

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -8,8 +8,8 @@ export class TodoItem extends Component {
         return (
             <li className="list-group-item text-capitalize d-flex justify-content-between my-2 border">
                 <h6>{title}</h6>
-                <div className="todo-icon" onClick={handleEdit}>
-                    <span className="mx-2 text-success">
+                <div className="todo-icon">
+                    <span className="mx-2 text-success" onClick={handleEdit}>
                         <i className="fas fa-pen"/>
                     </span>
                     <span className="mx-2 text-danger" onClick={handleDelete}>
